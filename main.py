07f1cef18fdf2d6a14093c5609af29da428c8f3e
Alex Xiong <awx1@rice.edu>
2019-12-20 01:07:22 -0500

    Update main.py
    not sure what to do next

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import datetime as dt
 import re
 import heapq
 import nltk
-nltk.download('averaged_perceptron_tagger')
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+import string
 
 reddit = praw.Reddit(client_id='F7D0F0RLEx5dOQ',
 					 client_secret='LJ6g4ADGtAFqDjkrOJ4Z9EekDMU',
@@ -21,13 +24,15 @@ top_subreddit = subreddit.top(limit = 500)
 for submission in subreddit.top(limit=1):
 	print(submission.title, submission.id)
 '''
-
+# Create storage for each post
 topics_dict = { "title":[],
 				"score":[],
 				"id":[], "url":[],
 				"comms_num": [],
-				"created": [],"body":[]}
+				"created": [],
+				"body":[]}
 
+# Parse through the top 500 posts in the subreddit
 for submission in top_subreddit:
     topics_dict["title"].append(submission.title)
     topics_dict["score"].append(submission.score)
@@ -37,27 +42,46 @@ for submission in top_subreddit:
     topics_dict["created"].append(submission.created)
     topics_dict["body"].append(submission.selftext)
 
-topics_data = pd.DataFrame(topics_dict)
+#topics_data = pd.DataFrame(topics_dict)
 #print(topics_data)
 
 commonWords = {}
 
-for firstSentence in topics_dict["title"] + topics_dict["body"]:
-	token = nltk.word_tokenize(firstSentence)
+print(type(topics_dict["title"]))
+print(topics_dict["title"][0])
+# Iterate through the two sentences
+for firstSentence in topics_dict["title"]:
+	token = word_tokenize(firstSentence)
+
+	# newToken = token[::]
+	# for elem in token:
+	# 	if elem in string.punctuation:
+	# 		newToken.remove(elem)
+
+	# tokenized = nltk.pos_tag(newToken)
+
 	tokenized = nltk.pos_tag(token)
+
+	# for word, wordToken in tokenized:
+	# 	if wordToken in ['PRP$', 'IN', 'DT']:
+	# 		pass
+	# 	else:
+	# 		if word not in commonWords:
+	# 			commonWords[word] = 1
+	# 		else:
+	# 			commonWords[word] += 1	
+	print(tokenized)
+	break
 	for word, wordToken in tokenized:
-		if wordToken in ['PRP$', 'IN', 'DT']:
-			pass
+		if word not in commonWords:
+			commonWords[word] = 1
 		else:
-			if word not in commonWords:
-				commonWords[word] = 1
-			else:
-				commonWords[word] += 1	
+			commonWords[word] += 1
 
 #topWords = dict(sorted(commonWords.iteritems(), key=operator.itemgetter(1), reverse=True)[:20])
 
 max(commonWords, key=commonWords.get)
-sorted(commonWords, key=commonWords.get, reverse=True)[:5]
+sorted(commonWords, key=commonWords.get, reverse=True)[:20]
 topWords = heapq.nlargest(20, commonWords, key=commonWords.get)
 
 print(topWords)
