765a7ad5cca485e33f286eaa3adcab4fcd0c751a
Alex Xiong <awx1@rice.edu>
2019-12-25 14:24:24 -0500

    Minor changes + contraction file

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ import nltk
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import RegexpTokenizer
+from contractions import CONTRACTION_MAP
 import string
+
+num = 501
 
 reddit = praw.Reddit(client_id='F7D0F0RLEx5dOQ',
 					 client_secret='LJ6g4ADGtAFqDjkrOJ4Z9EekDMU',
@@ -18,12 +21,8 @@ reddit = praw.Reddit(client_id='F7D0F0RLEx5dOQ',
 
 subreddit = reddit.subreddit('TwoSentenceHorror')
 
-top_subreddit = subreddit.top(limit = 500)
+top_subreddit = subreddit.top(limit = num)
 
-'''
-for submission in subreddit.top(limit=1):
-	print(submission.title, submission.id)
-'''
 # Create storage for each post
 topics_dict = { "title":[],
 				"score":[],
@@ -34,54 +33,74 @@ topics_dict = { "title":[],
 
 # Parse through the top 500 posts in the subreddit
 for submission in top_subreddit:
-    topics_dict["title"].append(submission.title)
-    topics_dict["score"].append(submission.score)
-    topics_dict["id"].append(submission.id)
-    topics_dict["url"].append(submission.url)
-    topics_dict["comms_num"].append(submission.num_comments)
-    topics_dict["created"].append(submission.created)
-    topics_dict["body"].append(submission.selftext)
+	if submission.title == "[deleted]":
+		pass
+	else:
+	    topics_dict["title"].append(submission.title)
+	    topics_dict["score"].append(submission.score)
+	    topics_dict["id"].append(submission.id)
+	    topics_dict["url"].append(submission.url)
+	    topics_dict["comms_num"].append(submission.num_comments)
+	    topics_dict["created"].append(submission.created)
+	    topics_dict["body"].append(submission.selftext)
 
-#topics_data = pd.DataFrame(topics_dict)
-#print(topics_data)
+topics_data = pd.DataFrame(topics_dict)
+print(len(topics_dict["title"]))
+print(len(topics_dict["body"]))
 
-commonWords = {}
+fullHorror = []
+for idx in range(len(topics_dict["title"])):
+	print(idx)
+	fullHorror.append([topics_dict["title"][idx], topics_dict["body"][idx]])
 
-print(type(topics_dict["title"]))
-print(topics_dict["title"][0])
-# Iterate through the two sentences
-for firstSentence in topics_dict["title"]:
-	token = word_tokenize(firstSentence)
+print(fullHorror)
 
-	# newToken = token[::]
-	# for elem in token:
-	# 	if elem in string.punctuation:
-	# 		newToken.remove(elem)
+'''
+Stuck right here. Not sure best way to pre-proccess for analysis or what to analyze for
+'''
 
-	# tokenized = nltk.pos_tag(newToken)
+def cleaner(sentence):
+	if sentence[0] == sentence[-1] and sentence[0] in ["'",'"',"*"]:
+		return sentence[1:len(sentence)-1]
+	elif sentence[-1] == ",":
+		return sentence[:len(sentence)-1]
+	else:
+		return sentence
 
-	tokenized = nltk.pos_tag(token)
+lastCharDict = {}
 
-	# for word, wordToken in tokenized:
-	# 	if wordToken in ['PRP$', 'IN', 'DT']:
-	# 		pass
-	# 	else:
-	# 		if word not in commonWords:
-	# 			commonWords[word] = 1
-	# 		else:
-	# 			commonWords[word] += 1	
-	print(tokenized)
-	break
-	for word, wordToken in tokenized:
-		if word not in commonWords:
-			commonWords[word] = 1
+def remove_special_characters(text):
+    pattern = r'[^a-zA-z0-9\s]'
+    text = re.sub(pattern, '', text)
+    return text.replace("\n",'')
+
+for first_sen in topics_data["title"]:
+	first_sen = cleaner(first_sen)
+	lastChar = first_sen[-1]
+	if lastChar in lastCharDict.keys():
+		lastCharDict[lastChar] += 1
+	else:
+		lastCharDict[lastChar] = 1
+
+lastCharDict2 = {}
+for first_sen in topics_data["body"]:
+	first_sen = remove_special_characters(first_sen)
+	#print(first_sen)
+	if first_sen == '':
+		pass
+	else:
+		lastChar = first_sen[-1]
+		if lastChar in lastCharDict2.keys():
+			lastCharDict2[lastChar] += 1
 		else:
-			commonWords[word] += 1
+			lastCharDict2[lastChar] = 1
+		if lastChar in ["*","'",",","]"]:
+			print(first_sen)
 
-#topWords = dict(sorted(commonWords.iteritems(), key=operator.itemgetter(1), reverse=True)[:20])
+'''
+https://towardsdatascience.com/a-practitioners-guide-to-natural-language-processing-part-i-processing-understanding-text-9f4abfd13e72
+'''
 
-max(commonWords, key=commonWords.get)
-sorted(commonWords, key=commonWords.get, reverse=True)[:20]
-topWords = heapq.nlargest(20, commonWords, key=commonWords.get)
+#print(lastCharDict2)
 
-print(topWords)
+
